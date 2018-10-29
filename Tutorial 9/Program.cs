a11ac24b8e5a13fc29aa94351b20d85e00c3dda2
Alberto Cervantes <a.cervantes@korvi.net>
2018-10-29 17:48:47 -0600

    video 9

---
+++ Tutorial 9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            clsClientesBase cliente = new clsClientesBase(0,
+                                                            "Alberto Cervantes", 
+                                                            "001",
+                                                            "CECJ770824mI5", 
+                                                            1, 
+                                                            "Alberto Cervantes");
+            Console.WriteLine(cliente.Clave + " " + cliente.Nombre);
+            Console.WriteLine(cliente.TipoRegimen);
+            Console.WriteLine(cliente.RFC);
+            Console.ReadKey();
+
+        }
+    }
+}
