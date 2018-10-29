30e491e0c40c696d974f4fce27260eb320415046
Alberto Cervantes <a.cervantes@korvi.net>
2018-10-29 16:05:51 -0600

    Video 6

---
+++ Tutorial 6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            clsEmpleados empleado;
+            empleado = new clsEmpleados();
+            empleado.Edad = 25;
+            empleado.Nombre = "Juan Perez";
+            empleado.SueldoDiario = 12.5m;
+            decimal total;
+            total = empleado.CalculaSalario(30);
+            Console.WriteLine("El salario mensual del empleado " + empleado.Nombre);
+            Console.WriteLine("es: " + total.ToString("C"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tutorial 6/clsEmpleados.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_6
+{
+    public class clsEmpleados
+    {
+        public clsEmpleados()
+        {
+            Nombre = "";
+            SueldoDiario = 0.0m;
+            Edad = 0;
+        }
+
+        public string Nombre;
+
+        public decimal SueldoDiario;
+
+        public int Edad;
+
+        public decimal CalculaSalario(int NumeroDias)
+        {
+            return SueldoDiario * NumeroDias;
+        }
+
+    }
+}
